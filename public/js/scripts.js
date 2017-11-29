46567b4b278fd16b81d78738313ea03980d8a669
Nik Bornemeier <NikolausRB@gmail.com>
2017-11-28 19:56:11 -0700

    add functionality to update hex value text

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,11 +1,24 @@
 const randomColor = () => {
   return '#' + Math.floor(Math.random() * 16777215).toString(16);
-}
+};
 
 const generateRandomColors = () => {
-  const newPalette = [randomColor(), randomColor(), randomColor(), randomColor(), randomColor()]
-  console.log(newPalette)
-}
+  const newPalette = [
+    randomColor().toUpperCase(), 
+    randomColor().toUpperCase(), 
+    randomColor().toUpperCase(), 
+    randomColor().toUpperCase(), 
+    randomColor().toUpperCase()];
+  updateColorText(newPalette);
+};
+
+const updateColorText = (newPalette) => {
+  $('.color-text-1').text(newPalette[0]);
+  $('.color-text-2').text(newPalette[1]);
+  $('.color-text-3').text(newPalette[2]);
+  $('.color-text-4').text(newPalette[3]);
+  $('.color-text-5').text(newPalette[4]);
+};
 
 $('.save-palette-button').on('click', () => {
   const paletteName = $('.palette-name-input').val();
@@ -19,7 +32,7 @@ $('.save-project-button').on('click', () => {
 
 $('.generate-palette-button').on('click', () => {
   generateRandomColors();
-})
+});
 
 
 
