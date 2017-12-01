48b7394858b3b8130c89db989934c56df7e261cc
Nik Bornemeier <NikolausRB@gmail.com>
2017-12-01 10:41:40 -0700

    setup heroku

--- knexfile.js
+++ knexfile.js
@@ -12,7 +12,15 @@ module.exports = {
       directory: './db/seeds/dev'
     },
     useNullAsDefault: true
-  }//,
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
+  }
   //test: {
   //
   //}
